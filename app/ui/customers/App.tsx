d2cdb1364f32daf8594c0dae2a6ab65d7c0aa0a9
lakshya104 <lakshya.vyas@anktech.co.in>
2023-12-08 11:47:05 +0530

    initial commit

---
+++ app/ui/customers/App.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import React, { useState } from 'react';
+
+const App = () => {
+  const [greeting, setGreeting] = useState<string>(
+    'Hello Function Component!'
+  );
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; })=>
+   setGreeting(event.target.value);
+
+  return (
+    <div>
+      <Headline headline={greeting}  />
+    <Input value={greeting} onChangeInput={handleChange}>
+    Set Greeting:
+  </Input>
+    </div>
+  );
+};
+
+interface HeadlineProps {
+    headline:string;
+    // onChangeHeadline: (e:any)=>any
+}
+
+const Headline:React.FC<HeadlineProps> = ({ headline }) => (
+  <div>
+    <h1>{headline}</h1>
+  </div>
+);
+
+interface InputProps {
+  value:string;
+  onChangeInput: (e:any)=>any;
+  children: React.ReactNode;
+}
+
+const Input:React.FC<InputProps> = ({ value, onChangeInput, children }) => (
+  <label>
+    {children}
+    <input type="text" value={value} onChange={onChangeInput} />
+  </label>
+);
+
+
+export default App;
